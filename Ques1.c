af7cc330d5b8681bff94bdec617c93e8c2ba5de0
neha-kumari01 <49370243+neha-kumari01@users.noreply.github.com>
2019-04-11 23:36:00 +0530

    Update Ques1.c

--- Ques1.c
+++ Ques1.c
@@ -1,20 +1,25 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
 
+//process body of process
 struct process
 {
 	int id;
 	int AT;
 	int BT,BT1;
-	int CT;
 	int WT;
+	int CT;
 	int TAT;
+	int check;   
 	int flag;
-	int check;
-} p[3];
-//declaring queue
+}p[4];
+
+void first_iteration();
+void second_iteration();
+int Index();
+void SJF();
+
+
+// declaring queue
 int ready_queue[150];
 int front=-1;
 int rear=-1;
@@ -24,142 +29,40 @@ void insert(int index)
         printf("Queue Overflow \n");
         else
         {
-        if (front == - 1)
+        if (front == -1)
         /*If queue is initially empty */
         front = 0;
         rear = rear + 1;
         ready_queue[rear] =index;
-        printf("Process entered readyQueue :%d\n",ready_queue[rear]);
+        printf("Process entered readyQueue: P%d\n",ready_queue[rear]);
         }
 }
 void qdelete()
 {
-	if (front == - 1 || front > rear)
+	if (front == -1 || front > rear)
     {
         printf("Queue Underflow \n");
         return ;
     }
     else
     {
-        printf("Element deleted from queue is : %d\n", ready_queue[front]);
+        printf("Element deleted from queue is: P%d\n", ready_queue[front]);
         front = front + 1;
     }
 }
 
+int time=0;
 int tq1=3;
 int tq2=6;
-float avgT=0;
-float avgW=0;
-int TotalWT=0;
-int TotalTAT=0;
+int TotalTIME=64;
 
-void firstIteration()
-{
-	int i=4,j,k;
-	int time=0;
-	insert(0);
-	p[0].check=1;
-	int n=ready_queue[rear];
-	while(n!=-1)
-	{
-	if(p[n].BT >= tq1 && p[n].BT>0)
-	{
-            printf("Process at time %d is P%d\n",time,n);			        
-	    p[n].BT=p[n].BT - tq1;
-		time+=tq1;
-		p[n].flag=1;   //counting number of times process execute
-		for(k=n+1;k<4;k++) //placing in ready queue
-		{
-		if(p[k].AT<time && p[k].check==0)
-		{
-		insert(k);
-		p[k].check++;
-    	}
-        }
-        if(p[n].BT>0)
-		insert(n);		
-					   
-	qdelete();
-	n=ready_queue[front];
-   }
-   
-   	if(p[0].flag==1)
-   	if(p[1].flag==1)
-   	if(p[2].flag==1)
-   	if(p[3].flag==1)
-   	{
-   	break;	
-   }
-}
-}
 
-void secondIteration()
-{
-	int k;
-	int n=ready_queue[front];
-	while(n!=-1)
-	{
-	if(p[n].BT >= tq2 && p[n].BT>0)
-	{
-        printf("Process at time %d is P%d\n",time,n);			        
-	    p[n].BT=p[n].BT - tq2;
-		time+=tq2;
-		p[n].flag=1;   //counting number of times process execute
-		
-        if(p[n].BT>0)
-	insert(n);		
-					   
-	qdelete();
-	n=ready_queue[front];
-   }
-   
-   	if(p[0].flag==1)
-   	if(p[1].flag==1)
-   	if(p[2].flag==1)
-   	if(p[3].flag==1)
-   	{
-   	break;	
-   }
-}
-}
-
-void SJF()
-{
-	int i;
-	int j=4;
-	int minBT=p[0].BT;
-	int index;
-	while(j!=0)
-	{
-	for(i=0;i<4;i++)
-	{
-	if(p[i].BT<minBT)
-	{
-	 index=i;
-	}
-	else if(p[index].BT == p[i].BT)
-	{
-		if(p[index].id>p[i].id)
-		{
-			index=i;
-		}
-	}
-	}
-	printf("Process at time %d is P%d",time,index);	
-	time+=p[index].BT;
-	 p[index].BT=0;
-		if(p[index].BT==0)
-		{
-			printf("\t Process P%d completes at time: %d",index,time);
-			p[index].CT=time;
-			j--;
-		}
-	}
-	
-}
-	
 int main()
 {
+	int TotalWT;
+	int TotalTAT;
+	float avgW;
+	float avgT;
 	int i;
 	// giving value of arrival time
 	p[0].AT=0;
@@ -178,6 +81,8 @@ int main()
 	for(i=0;i<4;i++)
 	{
 		p[i].id=i;
+		p[i].flag=0;
+		p[i].check=0;
 	}
 	
 	// keeping a backup of burst time
@@ -186,81 +91,182 @@ int main()
 		p[i].BT1=p[i].BT;
 	}
 	
-	printf("Process Id\t\t\t Arrival_TIME\t\t\tBURST_time\n"); 
+	//displaying the initial state
+	printf("Process Id\t Arrival_TIME\t BURST_time\n"); 
     for(i=0;i<4;i++)
     {
-     printf("P%d\t\t\t\t %d\t\t\t\t %d\n",i,p[i].AT,p[i].BT); 
+     printf("P%d\t\t %d\t\t %d\n",p[i].id,p[i].AT,p[i].BT); 
     }
     
-// round robin with Time quantum=3
-	firstIteration();
-	
-//displaying the scenario after first iteration
+    //round robin with timeQuantum 3
+    first_iteration();
+    
+    //displaying the scenario after first iteration
 	printf("Process Id\t\t\t Arrival_TIME\t\t\tBURST_time\t\t\tBURST_TIME_LEFT\n"); 
     for(i=0;i<4;i++)
     {
-     printf("P%d\t\t\t\t %d\t\t\t\t %d\t\t\t\t %d\n",i,p[i].AT,p[i].BT1,p[i].BT); 
+     printf("P%d\t\t\t\t %d\t\t\t\t %d\t\t\t\t %d\n",p[i].id,p[i].AT,p[i].BT1,p[i].BT); 
     }
-	
-// for each process changing flag value =0
-  for(i=0;i<4;i++)
-  {
+    
+    // for each process changing flag value =0 for second iteration
+    for(i=0;i<4;i++)
+    {
   	p[i].flag=0;
-   }
-   
-   //round robin with Time quantum=6
-   secondIteration();
-   
-   //displaying the scenario after first iteration
+    }
+    
+    //round robin with timeQuantum 6
+	second_iteration(); 
+		
+    //displaying the scenario after second iteration
 	printf("Process Id\t\t\t Arrival_TIME\t\t\tBURST_time\t\t\tBURST_TIME_LEFT\n"); 
     for(i=0;i<4;i++)
     {
-     printf("P%d\t\t\t\t %d\t\t\t\t %d\t\t\t\t %d\n",i,p[i].AT,p[i].BT1,p[i].BT); 
+     printf("P%d\t\t\t\t %d\t\t\t\t %d\t\t\t\t %d\n",p[i].id,p[i].AT,p[i].BT1,p[i].BT); 
     }
     
     //shortest job first
     SJF();
-	
-    printf("Process Id\t\t\t Arrival_TIME\t\t\tBURST_time\t\t\tBURST_TIME_LEFT\n"); 
+    
+    // calculating Waiting time and totaltime taken
     for(i=0;i<4;i++)
     {
-     printf("P%d\t\t\t\t %d\t\t\t\t %d\t\t\t\t %d\n",i,p[i].AT,p[i].BT1,p[i].BT); 
+    	p[i].TAT=p[i].CT-p[i].AT;
+    	p[i].WT=p[i].TAT-p[i].BT1;
+    	
+    	TotalWT=TotalWT+p[i].WT;
+    	TotalTAT=TotalTAT+p[i].TAT;
+	}
+	
+	//displaying the final scenario after completion of all processes
+    printf("Process Id\tArrival_TIME\tBURST_time\tCT\t\tWT\t\tTAT\n"); 
+    for(i=0;i<4;i++)
+    {
+     printf("P%d\t\t %d\t\t %d\t\t %d\t\t %d\t\t %d\n",p[i].id,p[i].AT,p[i].BT1,p[i].CT,p[i].WT,p[i].TAT); 
     }
+    
+    // calculating average waiting time and average turn around time
+    avgW=(float)(TotalWT/4);
+    avgT=(float)(TotalTAT/4);
+    printf("\nAVERAGE WAITING TIME : %f",avgW);
+    printf("\nAVERAGE TURNAROUND TIME : %f",avgT);
+    
+}
+
+void first_iteration()
+{
+	int i;
+	insert(0);
+	int n;
+	n=ready_queue[rear];
+	p[0].check=1;
+	
+	while(n!=-1)
+	{
+		if((p[n].BT>=tq1) && (p[n].BT>0))
+		{
+			printf("PROCESS EXECUTING AT TIME:%d is P%d\n",time,n);
+			p[n].BT = p[n].BT-tq1;
+			time=time+tq1;
+			//counting no.of times process executes
+			p[n].flag++;
+			//adding processes to ready queue
+			for(i=1;i<4;i++) // adding processes arriving while executing a process
+			{
+				if( (p[i].AT<time) && (p[i].check==0))
+				{
+					insert(i);
+					p[i].check++;
+				}
+			}
+			// inserting process if work is still left
+			if(p[n].BT>0)
+			{
+				insert(n);
+			}
+			//deleting a process who have executing this time
+			qdelete();
+			n=ready_queue[front];			
+		}
+	 if(p[0].flag>=1)
+	 if(p[1].flag>=1)
+	 if(p[2].flag>=1)
+	 if(p[3].flag>=1)
+	 {
+	 	break;
+	 }
+	}
+}
+
+void second_iteration()
+{
+	int i;
+	int n=ready_queue[front];
+	while(n!=-1)
+	{
+		if( (p[n].BT>= tq2)&&(p[n].BT>0) )
+		{
+        printf("Process at time %d is P%d\n",time,n);			        
+	    p[n].BT=p[n].BT-tq2;
+		time=time+tq2;
+		p[n].flag++;   //counting number of times process execute
+		
+        if(p[n].BT>0)
+        {
+		   insert(n);
+	    }
+					   
+	    qdelete();
+     	n=ready_queue[front];
+   }
    
-	//calculating values for Total Waiting time
-for(i=0;i<4;i++)
+   	if(p[0].flag>=1)
+   	if(p[1].flag>=1)
+   	if(p[2].flag>=1)
+   	if(p[3].flag>=1)
+   	{
+   	  break;	
+    }
+    }
+}
+
+int Index()
 {
-	//Total time taken=completion time-arrival time
-	p[i].TAT=p[i].CT - p[i].AT;
-	// waiting time= total time taken - burst time
-	p[i].WT= p[i].TAT -p[i].BT1;
+   int i;
+   int pos=0;
+   int min = p[0].BT;
+   for(i=0;i<4;i++)
+    {
+    	if(p[i].BT<min)
+    	{
+    		min=p[i].BT;
+    		pos=i;
+		}
+    }
+    	return pos;	
+}
+
+
+void SJF()
+{
+	int i=0;
+	int minBT=p[0].BT;
+	int index;
+	while(1)
+	{	
+	    index=Index();            
+		printf("Process at time %d is P%d\n",time,index);		
+		p[index].BT=p[index].BT-1;
+     	time=time+1;    	
+    	if(p[index].BT==0)
+		{
+			printf("\tProcess P%d completes at time: %d\n",index,time);
+			p[index].BT=16;
+			p[index].CT=time;
+		}
+		if(time==TotalTIME)
+		{
+			break;
+		}
+	}
 	
-	TotalWT= TotalWT+p[i].WT;
-	
-}
- 
-//calculating values for turn around time
-for(i=0;i<4;i++)
-{
-	TotalTAT= TotalTAT +p[i].TAT;
-}
-
-// displaying final scenario
-
-
-printf ("\nProcess_ID.\tAT\tBT\tWT\tTAT\tCT\n"); 
-for(i=0;i<3;i++)
-{
-printf ("%d\t %d\t %d\t %d\t %d\t %d\n",p[i].id,p[i].AT,p[i].BT,p[i].WT,p[i].TAT,p[i].CT); 
-}
-  
-printf("\nTOTAL WAITING TIME:%d\n",TotalWT);
-// average total waiting time
-avgw=(float)TotalWT/3;   
-printf("AVERAGE WAITING TIME:%f\n",avgw);
-printf("TURN AROUND TIME:%d\n",TotalTAT);
-float avgT=(float)TotalTAT/3;
-printf("AVERAGE TURN AROUND TIME:%f\n",avgT);
-
-
 }
